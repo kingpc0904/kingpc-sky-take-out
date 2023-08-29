0325bbe69031e0ec6966b652fe600fd0cade5d72
kingpc <3276363602@qq.com>
2023-08-29 20:56:10 +0800

    新增员工代码

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,6 +2,10 @@ package com.sky.context;
 
 public class BaseContext {
 
+    //ThreadLocal为每个线程提供单独一份的存储空间，具有线程隔离的效果，只有在线程内才能获取到对应的值，线程外获取不到。
+    //用户每次发送一个请求都是一个单独的线程，所以可以在解析jwt的时候顺便将用户id存储在ThreadLocal提供的存储空间内
+    //等运行到后面创建用户的时候，再将当前用户的id赋值给新建用户的createUser和updateUser.
+
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
     public static void setCurrentId(Long id) {
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -10,10 +13,12 @@ import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @Service
@@ -45,8 +50,6 @@ public class EmployeeServiceImpl implements EmployeeService {
         //密码比对
         //将前端传过来的明文密码通过md5加密，再与数据库的密码进行比对
         password = DigestUtils.md5DigestAsHex(password.getBytes());
-        log.info(password);
-        log.info(employee.getPassword());
         //当发现两个看似一样的字符串但是equals总是false时，可以通过Arrays.toString(字符串.toBytes)
 //        log.info(Arrays.toString(password.getBytes()));
 //        log.info(Arrays.toString(employee.getPassword().getBytes()));
@@ -62,6 +65,26 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        //对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //设置账号的状态，默认为开启
+        employee.setStatus(StatusConstant.ENABLE);
+        //设置账号的初始密码，默认为123456
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        //设置当前记录的创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置当前记录的创建者id和修改者id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.insert(employee);
     }
 
 }
